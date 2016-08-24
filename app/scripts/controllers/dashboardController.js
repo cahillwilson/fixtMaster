3f48235dbd81b80ce60e3204227920017dc25850
Sayantan Sen <sayantas@in.ibm.com>
2016-08-24 12:29:01 +0530

    SS: Correcting the code and revert back some lost code

--- app/scripts/controllers/dashboardController.js
+++ app/scripts/controllers/dashboardController.js
@@ -1,22 +1,17 @@
 'use strict';
 
-angular.module('fixtApp')    //Was trying to add the card module here
-    .controller('dashboardController', function (constantLoader, $log, $scope, $location) {
+angular.module('fixtApp')
+    .controller('dashboardController', function (constantLoader) {
 
     var vm =  this;
     vm.searchList = constantLoader.defaultObjects.SEARCH_LIST;
-    $scope.dashboard = {};
    
     function initialized() {
         
     }
     
     vm.onSearchItemClick = function(item){
-        $scope.dashboard.selectedSandbox = item;
-        $log.info(item);
-        console.log($scope.dashboard.selectedSandbox);
-        $location.url('/sandbox?id=' + item.id);
-        // change the route.
+        
     };
     
     initialized();
--- app/scripts/directives/cardDisplay.js
+++ app/scripts/directives/cardDisplay.js
@@ -1,17 +1,19 @@
-angular.module('fixtApp')
-	.directive('cardInfo', function(){
+'use strict';
 
-	return{
-		restrict:'EA',
-		replace: true,
-			scope:{
-				nodeId: "="
-			},
-		//transclued:true;
-		templateUrl: "../views/card_info.html",
-		controller: function($scope, $location){
-			var id = $location.search().id;
-			$scope.sandboxId = id;
-		}	
-	}
+angular.module('fixtApp')
+    .directive('cardInfo', function(){
+
+    return{
+            restrict:'EA',
+            replace: true,
+            scope:{
+                nodeId: "="
+            },
+            //transclued:true;
+            templateUrl: "../views/card_info.html",
+            controller: function($scope, $location){
+                    var id = $location.search().id;
+                    $scope.sandboxId = id;
+            }	
+    };
 });	
