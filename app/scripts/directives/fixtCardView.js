ad616e8d9d3d646406005cc5b43f7ffae721fa5e
Sayantan Sen <sayantas@in.ibm.com>
2016-08-30 21:20:46 +0530

    SS: Adding directive for card view

---
+++ app/scripts/directives/fixtCardView.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('fixtApp')
+  .directive('fixtCardView', function () {
+    return {
+	restricted: "E",
+	replace: true,
+        scope: {
+            
+        },
+        template: function(){
+            
+            var html =  '';
+        
+            return html;
+        }
+    };
+  });
