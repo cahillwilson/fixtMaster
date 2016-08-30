763aaf0023443f9fc63947adabd92c7df3467b65
Sayantan Sen <sayantas@in.ibm.com>
2016-08-30 18:48:37 +0530

    SS: Card controller update for demo

--- app/scripts/controllers/cardController.js
+++ app/scripts/controllers/cardController.js
@@ -7,23 +7,24 @@ angular.module('fixtApp')
     vm.title = constantLoader.defaultValues.SANDBOX_TITLE;
     vm.cardDetails = {};
     
-//    vm.cardDetails.items = ["C: Walmart 09854-DC", "D: I",
-//                            "E: 1717858114129", "F: UAMCITRIXDROIDLEAD",
-//                            "H: 124988287", "L: 19-APR-01"];
-    
+    vm.cardDetails.items = ["C: Walmart 09854-DC", "D: I",
+                            "E: 1717858114129", "F: UAMCITRIXDROIDLEAD",
+                            "H: 124988287", "L: 19-APR-01"];
+                        
+    vm.cardTitle = (vm.cardDetails.items.length>0) ? 
+        vm.cardDetails.items[vm.cardDetails.items.length-1]:
+        constantLoader.defaultValues.BLANK_STRING;
     
     function initialized() {
         loadCardDetails();
     }
     
     function loadCardDetails(){
-        cardBusiness.getCardDetailsListAsync().then(function(response){
-            vm.cardDetails = response.data;
-            
-//            vm.cardTitle = (vm.cardDetails.items.length>0) ? 
-//                vm.cardDetails.items[vm.cardDetails.items.length-1]:
-//                    constantLoader.defaultValues.BLANK_STRING;
-        }, handlerLoader.exceptionHandler.logError);
+//        cardBusiness.getCardDetailsListAsync().then(function(response){
+//            vm.cardDetails = response.data;
+//            
+//
+//        }, handlerLoader.exceptionHandler.logError);
     }
     
     initialized();
