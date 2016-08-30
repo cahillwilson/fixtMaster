cbe67627fa3d0e4424d4465bcd2dc2ae8ed8ea42
AMS <amshidda@us.ibm.com>
2016-08-29 23:24:13 -0400

    Adding inline comments for future reference

--- app/scripts/controllers/cardController.js
+++ app/scripts/controllers/cardController.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('fixtApp')
-    .controller('cardController', function (constantLoader) {
+    .controller('cardController', function (constantLoader, $http) {
 
     var vm =  this;
     vm.title = constantLoader.defaultValues.SANDBOX_TITLE;
@@ -9,6 +9,15 @@ angular.module('fixtApp')
     vm.cardDetails.items = ["C: Walmart 09854-DC", "D: I",
                             "E: 1717858114129", "F: UAMCITRIXDROIDLEAD",
                             "H: 124988287", "L: 19-APR-01"];
+
+    //$http.get('/scripts/services/json/invoiceNodeDetails.json',{cache: true}).then(function (resp) {
+    //    var data = resp.data;
+    //    var summary = data.invoiceNodeDetails.summary;
+        // $scope.cardScope = {};
+        // $scope.cardScope.myData = summary;
+        //$scope.myData = summary;
+        // html: {{cardScope.myData}}
+    //});
     vm.cardTitle = (vm.cardDetails.items.length>0) ? 
         vm.cardDetails.items[vm.cardDetails.items.length-1]:
             constantLoader.defaultValues.BLANK_STRING;
