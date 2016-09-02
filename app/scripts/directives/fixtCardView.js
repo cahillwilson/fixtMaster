c103f29864a9539c9266143871ed37d6445cfc92
Sayantan Sen <sayantas@in.ibm.com>
2016-09-02 14:46:57 +0530

    SS: Added new json files of heirarchy

--- app/scripts/directives/fixtCardView.js
+++ app/scripts/directives/fixtCardView.js
@@ -11,7 +11,6 @@ angular.module('fixtApp')
             cardLabel: "=",
             detailsClick: "&",
             hierarchyClick: "&"
-        
         },
         template: function(){
             
