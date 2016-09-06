ba35af9f8aedd4d4c03a225e4e20e9518ba02ff3
Monika <monika.murmu@in.ibm.com>
2016-09-06 18:22:52 +0530

    MT: Fixed cyclic dependency

--- app/scripts/services/handler/modalHandler.js
+++ app/scripts/services/handler/modalHandler.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('fixtApp')
-  .factory('modalHandler', function (ngDialog, commonUtility, constantLoader) {
+  .factory('modalHandler', function (ngDialog, constantLoader) {
     
     var modalDialog = {};
 
@@ -16,12 +16,14 @@ angular.module('fixtApp')
                 closeByDocument: false
             });
         };
-        modalDialog.showConfirm = function(message, positiveText, negetiveText){
+        modalDialog.showConfirm = function(message, positiveText, negativeText){
             
-            positiveText = (commonUtility.is3DValidKey(positiveText))? 
-                positiveText : constantLoader.defaultValues.CONFIRM_BOX_YES;
-            negetiveText = (commonUtility.is3DValidKey(negetiveText))? 
-                negetiveText : constantLoader.defaultValues.CONFIRM_BOX_NO;
+            positiveText = (angular.isDefined(positiveText) &&
+                    positiveText !== constantLoader.defaultValues.BLANK_STRING && positiveText !== null) ?
+                    positiveText : constantLoader.defaultValues.CONFIRM_BOX_YES;
+            negativeText = (angular.isDefined(negativeText) &&
+                    negativeText !== constantLoader.defaultValues.BLANK_STRING && negativeText !== null) ?
+                    negativeText : constantLoader.defaultValues.CONFIRM_BOX_NO;
             
             var nestedConfirmDialog = ngDialog.openConfirm({
                 template:   '<p>' + message + '</p>' +
@@ -29,7 +31,7 @@ angular.module('fixtApp')
                                 '<button type="button" class="ngdialog-button ngdialog-button-primary" ' +
                                     ' ng-click="confirm(1)">' + positiveText + '</button>' +
                                 '<button type="button" class="ngdialog-button ngdialog-button-secondary" ' +
-                                    'ng-click="closeThisDialog(0)">' + negetiveText + '</button>' +
+                                    'ng-click="closeThisDialog(0)">' + negativeText + '</button>' +
                             '</div>',
                 plain: true,
                 closeByDocument: false
