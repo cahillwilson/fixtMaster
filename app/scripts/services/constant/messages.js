ef4a77714704a32a7c735e526501611cf532e3d3
Monika <monika.murmu@in.ibm.com>
2016-09-06 18:20:32 +0530

    MT: Code cleanup

--- app/scripts/services/constant/messages.js
+++ app/scripts/services/constant/messages.js
@@ -4,14 +4,5 @@ angular.module('fixtApp')
     .constant('messages', {
         SEARCH_NOT_VALID: "Please enter a valid search term",
         SEARCH_ITEM_NOT_VALID: "Please enter a valid search item",
-        SEARCH_VALIDATION_CUSTOMER: "search customer",
-        SEARCH_VALIDATION_MCN: "search mcn",
-        SEARCH_VALIDATION_VENDOR: "search vendor",
-        SEARCH_VALIDATION_CSS: "search css",
-        SEARCH_VALIDATION_FOREIGN_ACCOUNT: "search foreign acc",
-        SEARCH_VALIDATION_CONTRACT: "search contract",
-        SEARCH_VALIDATION_ADDRESS: "search address",
-        SEARCH_VALIDATION_INVENTORY: "search inventory",
-        SEARCH_VALIDATION_ACCOUNT: "search account",
         SEARCH_ERROR_MSG: "Please enter a valid "
     });
--- app/scripts/services/constant/validationPatterns.js
+++ app/scripts/services/constant/validationPatterns.js
@@ -2,16 +2,7 @@
 
 angular.module('fixtApp')
   .constant('validationPatterns', {
-        SEARCH_CUSTOMER: /^[a-zA-Z]*$/,
-        SEARCH_MCN: /^[a-zA-Z]*$/,
-        SEARCH_VENDOR: /^[a-zA-Z]*$/,
-        SEARCH_CSS: /^[a-zA-Z]*$/,
-        SEARCH_FOREIGN_ACCOUNT: /^[a-zA-Z]*$/,
-        SEARCH_CONTRACT: /^[a-zA-Z]*$/,
-        SEARCH_ADDRESS: /^[a-zA-Z]*$/,
-        SEARCH_INVENTORY: /^[a-zA-Z]*$/,
-        SEARCH_ACCOUNT: /^[a-zA-Z]*$/,
         ALPHA_NUMERIC: /^[0-9a-zA-Z]+$/,
-        NUMERIC:/^[0-9]*$/,
+        NUMERIC:/^[0-9]*$/
         
   });
