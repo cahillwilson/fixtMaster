9cb7395d844b3fa50d1b620c00f5e68d25e99168
IBM216-PC09K5XP\AISHA <amshidda@us.ibm.com>
2016-09-07 17:36:29 -0400

    AMS: Removing textbox

--- app/scripts/directives/fixtExpandedHierarchy.js
+++ app/scripts/directives/fixtExpandedHierarchy.js
@@ -15,7 +15,7 @@ angular.module('fixtApp')
   <div class="sBxbox1-2"><span id='close'><img src="styles/images/btn-close-sml.png" width="12" height="10" alt=""/></span></div>
  
 	<div class="sBxheader">
-		<div class="sBxtitle"> <span id="itm1" onclick="exchange(this.id)">Walmart SA12345678123- State DC</span><input id="itm1b" class="replace" type="text" value="" placeholder="I"></div>
+		<div class="sBxtitle"> <span id="itm1" onclick="exchange(this.id)">Walmart SA12345678123- State DC</span></div>
 <div class="sBxaction">
 			<div class="sBxbox1-1">
             
@@ -40,24 +40,21 @@ angular.module('fixtApp')
         <div class="hrNode1-2"><span class="nodeIcon"><img src="styles/images/Hrchy-collapse.jpg" width="20" height="20" alt=""/></span>H 98564321 - East region</div>
  		<div class="hrNode1-3"><span class="nodeIcon"><img src="styles/images/Hrchy-collapse.jpg" width="20" height="20" alt=""/></span>BA 1234567891234-East region</div>
         <div class="hrNode1-4"><span class="nodeIcon"><img src="styles/images/Hrchy-collapse.jpg" width="20" height="20" alt=""/></span>1:1122334455667-East </div>
-        <div class="hrNode1-5"><span class="nodeIcon"><img src="styles/images/Hrchy-collapse.jpg" width="20" height="20" alt=""/></span>1:1122334455667-East <span class="badge">234</span></div>
+        <div class="hrNode1-5"><span class="nodeIcon"><img src="styles/images/Hrchy-collapse.jpg" width="20" height="20" alt=""/></span>1:1122334455667-East </div>
         <div class="hrNode1-6"><span class="nodeIcon"><input type="image" src="styles/images/Hrchy-expand.jpg" width="20" height="20" alt="expand"/></span>Sub Accounts 001-100</div>
-        
-        <div class="hrNode2-1"><span class="nodeIcon"><div class="expandNode" ng-repeat="children in myList"><img src="styles/images/Hrchy-child.jpg" width="10" height="10" alt=""/>{{children}}</div></span></div>
+        <div class="hrNode2-1"><span class="nodeIcon"><div ng-repeat="children in myList"><img src="styles/images/Hrchy-child.jpg" width="10" height="10" alt=""/>{{children}}</div></span></div>
         		
-               
                 
-        
-        
 	</div>
     <div class="sbxHrchyFooter">
-        	<div class="sbxHrchyFtrLeft"><a href="#/sandbox">Back to card view</a></div>
+        	<div class="sbxHrchyFtrLeft"><a href="#">Back to card view</a></div>
             <div class="sbxHrchyFtrRight"><a href="#">Open in new card</a></div>
         </div><!-- sBxfooter end-->
 
 </div> 
 `
 return html;
+
 	
 },
 }
@@ -66,3 +63,5 @@ return html;
 
 
 });
+
+
