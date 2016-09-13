85fb5dfbaceee9594833153afcec31a5338c8088
Monika <monika.murmu@in.ibm.com>
2016-09-13 19:33:08 +0530

    MT: Search summary component

--- app/scripts/directives/fixtSearchList.js
+++ app/scripts/directives/fixtSearchList.js
@@ -6,12 +6,34 @@ angular.module('fixtApp')
 	restricted: "E",
 	replace: true,
         scope: {
+            searchList: "=",
             
         },
         template: function(){
             
-            var html =  '';
-        
+            var html =  
+            '<div ng-repeat="item in searchList">' +
+                    '<div class="srchInfo">' +
+                        '<div class="srchTtlDtls">' +
+                            '<div class="checkbox checkbox-primary">' +
+                                '<input id="checkbox_{{$index}}" type="checkbox">' +  
+                                '<label for="checkbox_{{$index}}">' + 
+                                    '<div class="srchDtCnt-a">{{item.nodeLabel}} {{item.nodeType}} {{item.nodeID}}</div>' +
+                                    '<span class="srchDtCnt-b">ID</span>' +
+                                    '<span class="srchDtCnt-C">{{item.nodeID}}</span>' +
+                                    '<span class="srchDtCnt-b">Label</span>' +
+                                    '<span class="srchDtCnt-C">{{item.nodeLabel}}</span>' +
+                                    '<span class="srchDtCnt-b">Start Date</span>' +
+                                    '<span class="srchDtCnt-C">{{item.startDate | fixtSplit:\' \' : 0|date: \'MM/dd/yyyy\'}}</span>' +
+                                    '<span class="srchDtCnt-b">Address</span>' +
+                                    '<span class="srchDtCnt-C">123 Main street Dallas TX12345</span>' + 
+                                '</label>' +
+                            '</div>' +
+                        '</div>' +
+                        '<span class="expandInfo"><img src="styles/images/Hrchy-expand.jpg" width="20" height="20" alt=""/></span>' +
+                    '</div>' +
+                    '<div class="brdrInf"></div>' +
+                '</div>';
             return html;
         },
         link: function(scope){
