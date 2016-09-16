664c9424cc3b0136b30bfa3749fd7deb43212fc8
Sayantan Sen <sayantas@in.ibm.com>
2016-09-16 21:10:20 +0530

    SS: searchlist pagination cont...

--- app/scripts/directives/fixtSearchListPagination.js
+++ app/scripts/directives/fixtSearchListPagination.js
@@ -12,15 +12,38 @@ angular.module('fixtApp')
             
             var html =  '<div class="srchInFooter">' +
                             '<ul class="srchInFooter">' +
-                                '<li class="Pgnsn">' +
-                                    '<img src="styles/images/arrow_gry-lft.png" width="5" height="8" alt=""/>' +
+                                '<li class="Pgnsn1 pointer" ng-click="onLeftClick()">' +
+                                    '<span ng-if="activePage>1">' +
+                                        '<img ng-src="styles/images/arrow_blu-lft.png" width="5" height="8" alt=""/>' +
+                                    '</span>' +
+                                    '<span ng-if="activePage===1">' +
+                                        '<img ng-src="styles/images/arrow_gry-lft.png" width="5" height="8" alt=""/>' +
+                                    '</span>' +
                                 '</li>' +
-                                '<li class="Pgnsn" ng-repeat="page in [] | fixtRange: totalRecord/10 | limitTo: 8">{{page}}</li>' +
-                                '<li class="Pgnsn" ng-if="(totalRecord/10)>10">...</li>' +
-                                '<li class="Pgnsn" ng-if="(totalRecord/10)>10" ' +
-                                    'ng-repeat="page in [] | fixtRange: totalRecord/10 | limitTo: -2">{{page}}</li>' +
-                                '<li class="Pgnsn">' +
-                                    '<img src="styles/images/arrow_blu-rt.png" width="5" height="8" alt=""/>' +
+                                '<li class="Pgnsn pointer" ng-repeat="page in [] | fixtRange: pageCount | ' +
+                                    'limitTo: 2" ng-class="{\'pg-acton\':activePage==={{page}}}" ng-click="onPageClick(page)">' +
+                                    '{{page}}' +
+                                '</li>' +
+                                '<li class="Pgnsn1" ng-if="((pageCount>10) && activePage>8)">...</li>' +
+                                '<li class="Pgnsn pointer" ng-repeat="page in [] | fixtRange: pageCount-2 | ' +
+                                    'limitTo: ((activePage<9)?((pageCount>7)?8:pageCount):activePage) | limitTo: -6" ng-class="{\'pg-acton\':activePage==={{page}}}" ' +
+                                    'ng-click="onPageClick(page)">' +
+                                    '{{page}}' +
+                                '</li>' +
+                                '<li class="Pgnsn1" ng-if="((pageCount>10) && activePage<(pageCount - 2))">...</li>' +
+                                '<li class="Pgnsn pointer" ng-if="(pageCount)>10" ' +
+                                    'ng-repeat="page in [] | fixtRange: pageCount | ' +
+                                    'limitTo: -2" ng-class="{\'pg-acton\':activePage==={{page}}}" ' +
+                                    ' ng-click="onPageClick(page)">' +
+                                    '{{page}}' +
+                                '</li>' +
+                                '<li class="Pgnsn1 pointer" ng-click="onRightClick()">' +
+                                    '<span ng-if="activePage<pageCount">' +
+                                        '<img ng-src="styles/images/arrow_blu-rt.png" width="5" height="8" alt=""/>' +
+                                    '</span>' +
+                                    '<span ng-if="activePage===pageCount">' +
+                                        '<img ng-src="styles/images/arrow_gry-rt.png" width="5" height="8" alt=""/>' +
+                                    '</span>' +
                                 '</li>' +
                             '</ul>' +
                             '<div class="srchInFtrBox cmnFrteen">' +
@@ -42,8 +65,24 @@ angular.module('fixtApp')
             return html;
         },
         link: function(scope){
+            scope.activePage = 1;
+            scope.pageCount = (scope.totalRecord / 10) + ((scope.totalRecord%10) > 0 ? 1 : 0);
             
+            scope.onPageClick = function(page){
+                scope.activePage = Number(page);
+            };
             
+            scope.onLeftClick = function(){
+                if(scope.activePage>1){
+                    scope.activePage = scope.activePage - 1;
+                }
+            };
+            
+            scope.onRightClick = function(){
+                if(scope.activePage<scope.pageCount){
+                    scope.activePage = scope.activePage + 1;
+                }
+            };
         }
     };
   });
